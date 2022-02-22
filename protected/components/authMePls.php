3dd1dc950e01c5d2cdcaa20f0eaee088f4b13bfd
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 21:31:29 +0100

    Create authMePls.php

---
+++ protected/components/authMePls.php
@@ -0,0 +1,21 @@
+<?php
+$u = filter_var($_POST["username"], FILTER_SANITIZE_STRING);
+$p = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+
+if (empty($u)) {
+  header("Location: /login");
+}
+
+if (empty($p)) {
+  header("Location: /login");
+}
+
+// Ok, ora accedo a nPay
+$response = json_decode(file_get_contents("https://sunfire.a-centauri.com/npayapi/?richiesta=estratto&utente=$u&auth=$p"));
+
+if ($response->status == 200) {
+  $_SESSION["user"] = $u;
+  header("Location: /dashboard/");
+} else {
+  header("Location /login");
+}
