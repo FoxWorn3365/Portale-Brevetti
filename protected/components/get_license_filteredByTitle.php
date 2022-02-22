e8e318e573b5db776d6beb5f022b5f4ed6bb7022
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 20:21:05 +0100

    Create get_license_filteredByTitle.php

---
+++ protected/components/get_license_filteredByTitle.php
@@ -0,0 +1,20 @@
+<?php
+use Brevetti\FileGo;
+require_once("protected/autoload.php");
+
+$b = new FileGo();
+
+foreach(glob("protected/brevetti/*") as $file) {
+  $filename = str_replace("protected/brevetti/", "", $filename);
+  $get = $b->getLicense($file);
+  $c = $b->loadStatusColor();
+  if ($get !== 502 && stripos($b[1], $title) !== false) {
+    ?>
+    <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
+    <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
+    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $c[$get[4]]; ?>'><?= $get[4]; ?></span></span>
+    </a>
+    <br><hr><br>
+    <?php
+  }
+}
