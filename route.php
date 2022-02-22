8ea3991d43079a8bbe19dab69fcb94618aed2746
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 20:29:28 +0100

    Update route.php

--- route.php
+++ route.php
@@ -3,4 +3,12 @@ $url = $_SERVER["REQUEST_URI"];
 
 if (empty($url)) {
   require_once("protected/pages/main.php");
+} elseif ($url == "/login") {
+  require_once("protected/components/header.php");
+  require_once("protected/pages/login.php");
+} elseif ($url == "/auth") {
+  require_once("protected/components/header.php");
+  require_once("protected/components/authMePls.php");
+} elseif ($url = "/antifortnine/") {
+  header("Location: https://pornhub.com/");
 }
