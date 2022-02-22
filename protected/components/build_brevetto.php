66f64006c09cf570407410eb946f3f7bbd89785d
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 22:47:26 +0100

    Update build_brevetto.php

--- protected/components/build_brevetto.php
+++ protected/components/build_brevetto.php
@@ -8,7 +8,7 @@ $get = $b->getPatent($brevetto);
 $color = $b->loadStatusColor();
 $name = $b->loadNameFromStatus();
 ?>
-     <h2><?= $get[2]; ?></h2>
+     <h2><?= $get[2]; ?> - ID: <?= $brevetto; ?></h2>
      <hr>
      <span style='font-size: 20px'><b>Autore:</b> <?= $get[0]; ?><img src="https://minotar.net/helm/<?= $get[0]; ?>/100.png"> | Creato in data: <?= date("d/m/Y - H:i:s", $get[1]); ?> | Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span><br><hr><br>
      <pre><?= $get[3]; ?></pre>
