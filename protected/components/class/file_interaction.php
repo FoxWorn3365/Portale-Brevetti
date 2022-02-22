48a99a6707ede59e7a1efe54a76a13e8e82a4344
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 19:41:40 +0100

    Update file_interaction.php

--- protected/components/class/file_interaction.php
+++ protected/components/class/file_interaction.php
@@ -31,12 +31,42 @@ class FileGo {
   
   public function getLicenseStatus($id) {
     $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    if (!file_exists("protected/brevetti/$id")) {
+      return 502;
+    }
     $tempStatus = explode("{|}", file_get_contents("protected/brevetti/$id"));
     return $tempStatus[3];
   }
   
   public function setLicenseStatus($id, $status) {
     $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    $status = filter_var($status, FILTER_SANITIZE_NUMBER_INT);
     // 0 = in approvazione; 1 = approvato; 2 = negato; 3 = scaduto (teoricamente messo in automatico dal server
-    
+    if ($status !== 0 && $status !== 1 && $status !== 2 && $status !== 3) {
+      return 501;
+    }    
+    if (!file_exists("protected/brevetti/$id")) {
+      return 502;
+    }
+    // Ok, l'input è valido, modifico il file
+    $brevetto = explode("{|}", file_get_contents("protected/brevetti/$id"));
+    $newSubString = $brevetto[0] . '{|}' . $brevetto[1] . '{|}' . $brevetto[2] . '{|}' . $brevetto[3] . '{|}' . $status;
+    file_put_contents("protected/brevetti/$id", $newSubString);
+    if ($this->getLicenseStatus($id) === $id) {
+      return 200;
+    } else {
+      return 500;
+    }
+  }
+  
+  public function getLicense($id) {
+    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    if (!file_exists("protected/brevetti/$id")) {
+      return 502;
+    } else {
+      return explode("{|}", file_get_contents("protected/brevetti/$id"));
+    }
+  }
+  
+  
     
