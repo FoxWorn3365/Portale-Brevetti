277818e5c6fbf8ed75e6fa850076d41c2874af87
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 21:50:19 +0100

    Update dashboardMain.php

--- protected/pages/dashboardMain.php
+++ protected/pages/dashboardMain.php
@@ -9,3 +9,7 @@ $b = new FileGo();
      Da questo pannello potrai creare e gestire i tuoi brevetti
      <br><br><br>
      <a href='/dashboard/mep'><button class="w3-button w3-green w3-text-white">I tuoi Brevetti</button></a><a href='/dashboard/nuovobrevetto'><button class="w3-button w3-green w3-text-white">Nuovo Brevetto</button></a><a href='/logout'><button class="w3-button w3-red w3-text-white">Esci</button></a>
+     <br><br>
+     <h3>Brevetti</h3>
+     <progress max="5" value="<?= $b->getUserPatentCount($_SESSION["user"]); ?>"></progress>
+     <br><br><br>
