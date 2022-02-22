51ab473e937c38963a6ab038a01ead64f2c0d4ed
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 20:24:40 +0100

    Update main.php

--- protected/pages/main.php
+++ protected/pages/main.php
@@ -1,3 +1,9 @@
-    <!-- Da qua -->
+<?php
+require_once("protected/components/header.php");
+?>
+   <!-- Da qua -->
     una pagina index di esempio
     non saprei
+<?php
+require_once("protected/components/footer.php");
+?>
