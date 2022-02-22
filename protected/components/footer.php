10c2ed8cff296af60ab0716668dcfabcd8166991
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 20:26:37 +0100

    Update footer.php

--- protected/components/footer.php
+++ protected/components/footer.php
@@ -1,1 +1,7 @@
-Made with <3 by Foxworn3365
+  </body>
+  <footer>
+    <br>
+    <hr>
+    <h6><a href='https://rgbcraft.com/'>RGBCraft</a> - <a href='https://github.com/FoxWorn3365/Portale-Brevetti'>Portale Brevetti</a> - Made with <3 by FoxWorn3365 & PHP</h6>
+  </footer>
+</html>
