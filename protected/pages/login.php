f1b7cbb17400febe437c2d00a587dcfba67d3ed1
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-22 21:26:17 +0100

    Create login.php

---
+++ protected/pages/login.php
@@ -0,0 +1,14 @@
+<?php
+if (!empty($_SESSION["user"])) {
+  header("Location: /dashboard/");
+  exit;
+}
+?>
+     <h2>Accedi al Portale con nPay</h2>
+     <form method="post" action="/auth">
+       <b>Username:</b><br>
+       <i class="fa fa-user-circle-o" aria-hidden="true"></i><input type="text" name="username"><br>
+       <b>Password:</b><br>
+       <i class="fa fa-key" aria-hidden="true"></i><inpyt type="password" name="password"><br><br>
+       <button class="w3-button w3-orange w3-text-white">Accedi</button>
+     </form>
