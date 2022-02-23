fb42c1453df1a0a70abaa608460041963d094c6e
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-23 14:57:35 +0100

    Create admin_brevettilist.php

---
+++ protected/pages/admin_brevettilist.php
@@ -0,0 +1,21 @@
+<?php
+// Implementazione del FilterBy->get
+$filter = $_GET["filter"];
+$query = $_GET["q"];
+
+// Ora carichiamo lo script per generare la directory
+foreach(glob("protected/brevetti/*") as $file) {
+  $filename = str_replace("protected/brevetti/", "", $filename);
+  $get = $b->getPatent($file);
+  $c = $b->loadStatusColor();
+  $name = $b->loadNameFromStatus();
+  if ($get !== 502) {
+    ?>
+    <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
+    <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
+    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span>
+    </a>
+    <br><hr><br>
+    <?php
+  }
+}
