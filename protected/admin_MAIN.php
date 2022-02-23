e3b26eafe12b16abb182216cdf89963909352e00
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-23 14:53:36 +0100

    Create admin_MAIN.php

---
+++ protected/admin_MAIN.php
@@ -0,0 +1,7 @@
+     <h2>Portale Brevetti - zona Admin</h2>
+     <hr>
+     <br><br>
+     <h3>Benvenuto, <?= $_SESSION["user"]; ?></h3>
+     <br><br>
+     <h4>Azioni possibili:</h4>
+     <a href='/admin/brevetti'><button class='w3-button w3-green w3-text-white' syle='border-radius: 50px'>Lista dei Brevetti</button></a>     <a href='/dashboard/'><button class='w3-button w3-green w3-text-white' syle='border-radius: 50px'>Dashboard</button></a>
