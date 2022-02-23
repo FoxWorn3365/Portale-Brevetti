6e3b11482dcd0fd49a9add25f6c66604f18dc18f
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-23 15:02:50 +0100

    Update admin_brevettilist.php

--- protected/pages/admin_brevettilist.php
+++ protected/pages/admin_brevettilist.php
@@ -13,8 +13,7 @@ foreach(glob("protected/brevetti/*") as $file) {
     ?>
     <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
     <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
-    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span>
-    </a>
+    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span></a>  <a href='/admin/modifica/<?= $file; ?>'><button clss='w3-button w3-orange w3-text-white' style='border-raidus: 50px'>Modifica</button></a>
     <br><hr><br>
     <?php
   }
