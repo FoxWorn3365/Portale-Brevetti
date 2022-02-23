297d4c3e0584d0d45440858ae4e16ff6f8d2c298
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-23 14:59:58 +0100

    Update get_patent_list.php

--- protected/components/get_patent_list.php
+++ protected/components/get_patent_list.php
@@ -8,12 +8,12 @@ foreach(glob("protected/brevetti/*") as $file) {
   $filename = str_replace("protected/brevetti/", "", $filename);
   $get = $b->getPatent($file);
   $c = $b->loadStatusColor();
+  $name = $b->loadNameFromStatus();
   if ($get !== 502) {
     ?>
     <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
     <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
-    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $c[$get[4]]; ?>'><?= $get[4]; ?></span></span>
-    </a>
+    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span></a>
     <br><hr><br>
     <?php
   }
