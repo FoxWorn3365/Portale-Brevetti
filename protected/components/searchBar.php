03d776bd9d298521d7d014b55ffa2ad529a30c91
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-25 16:56:17 +0100

    Create searchBar.php

---
+++ protected/components/searchBar.php
@@ -0,0 +1,9 @@
+     <br><br>
+     <form method="post" action="/brevetti">
+       <input type="text" name="input">&nbsp;&nbsp;&nbsp;
+       <select name="do">
+         <option value="author">Per Autore</option>
+         <option value="title">Per Titolo</option>
+       </select>
+       &nbsp;&nbsp;&nbsp;<button class='w3-button w3-green w3-text-white w3-hover-white'>Filtra</button>
+     </form>
