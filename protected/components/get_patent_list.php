f00bfdd3050511f1f987b3a939f8fecb2a666d88
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-25 16:51:21 +0100

    Update get_patent_list.php

--- protected/components/get_patent_list.php
+++ protected/components/get_patent_list.php
@@ -1,8 +1,11 @@
 <?php
+require_once("protected/components/searchBar.php");
 use Brevetti\FileGo;
 require_once("protected/autoload.php");
 
 $b = new FileGo();
+$do = filter_var($_POST["do"], FILTER_SANITIZE_STRING);
+$input = filter_var($_POST["input"], FILTER_SANITIZE_STRING);
 
 foreach(glob("protected/brevetti/*") as $file) {
   $filename = str_replace("protected/brevetti/", "", $filename);
@@ -10,6 +13,21 @@ foreach(glob("protected/brevetti/*") as $file) {
   $c = $b->loadStatusColor();
   $name = $b->loadNameFromStatus();
   if ($get !== 502) {
+    if (empty($do)) {
+    ?>
+    <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
+    <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
+    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span></a>
+    <br><hr><br>
+    <?php
+    } elseif ($do == "author" && stripos($get[0], $input) !== false) {
+    ?>
+    <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
+    <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
+    <span style='font-size: 18px'>Di: <?php if ($get[0] == "guest") { echo 'pubblico dominio'; } else { echo $get[0]; } ?>&nbsp;&nbsp;&nbsp; Stato: <span style='color: <?= $color[$get[4]]; ?>'><?= $name[$get[4]]; ?></span></span></a>
+    <br><hr><br>
+    <?php
+    } elseif ($do == "title" && stripos($get[2], $input) !== false) {
     ?>
     <a style='text-decoration: none; color: black' href='/brevetto/<?= $file; ?>'>
     <span style='font-size: 30px'><b><?= $get[2]; ?></b></span><br>
